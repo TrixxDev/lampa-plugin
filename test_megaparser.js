12af84be5a0c2e83bc6558b61c7557fc6394f315
Edgars <indrikis38@gmail.com>
2025-12-27 21:17:55 +0200

    Test version of MegaParser

--- test_megaparser.js
+++ test_megaparser.js
@@ -1,22 +1,24 @@
 (function() {
-    var mod_version = '1.0.0 (27.12.2025)';
+    var version = '1.0.0';
 
-    console.log('MegaParser v' + mod_version + ' загружен');
+    console.log('MegaParser v' + version + ' успешно загружен!');
 
+    // Регистрация компонента (обязательно!)
     Lampa.Component.add('megaparser', function(object) {
         return {
             create: function() {
-                var html = $('<div>Тестовый MegaParser работает!</div>');
+                var html = $('<div>Тестовый MegaParser работает! Версия: ' + version + '</div>');
                 return html;
             }
         };
     });
 
+    // Регистрация в манифесте (чтобы отображалось в списке плагинов)
     Lampa.Manifest.plugins = {
         type: 'video',
-        version: mod_version,
+        version: version,
         name: 'MegaParser',
-        description: 'Мой парсер — работает!',
+        description: 'Мой тестовый парсер — работает!',
         component: 'megaparser'
     };
 })();
